e6383c8cc09637ef4d9cae5e3e0a378f79d78b6f
Alice (MikhalovskyMY) <alice@example.com>
2022-02-28 10:07:57 +0300

    code: добавлен вывод разности введенных чисел

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-    cout << "Sum: " << a + b << endl;
 
+    cout << "Sum: " << a + b << endl;
+    cout << "Diff: " << a - b << endl;
 
     return 0;
 }
