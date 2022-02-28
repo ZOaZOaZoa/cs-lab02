395b6ace7c595d6213da96a154e758fee478dea3
Alice (MikhalovskyMY) <alice@example.com>
2022-02-28 20:19:58 +0300

    code: добавлена печать максимума

--- main.cpp
+++ main.cpp
@@ -9,10 +9,21 @@ int main()
     int a, b;
     cin >> a >> b;
 
+    int max;
+    if (a >= b)
+    {
+        max = a;
+    }
+    else
+    {
+        max = b;
+    }
+
     cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
-     << "A / B = " << a / b << '\n';
+     << "A / B = " << a / b << '\n'
+     << "MAX = " << max << '\n';
 
     return 0;
 }
